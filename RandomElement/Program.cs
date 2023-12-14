db73cebd69631f79174416b679f58f8f17288ef9
BearTheWhale <markuskaru6@gmail.com>
2023-12-14 21:40:58 +0200

    add Week 6

---
+++ RandomElement/Program.cs
@@ -0,0 +1,24 @@
+﻿//string[] snacks = { "Leb", "sai", "õlu", "burger", "pitsa" };
+
+//Random rnd = new Random();
+
+//int randomIndex = rnd.Next(0, snacks.Length);
+
+//Console.WriteLine($"We are going to eat {snacks[randomIndex]}.");
+
+string hello = "Hello, world!".Trim();
+int spaceCounter = 0;
+
+for (int i = 0; i < hello.Length; i++)
+
+{
+
+    if (char.IsWhiteSpace(hello[i]))
+{ 
+        spaceCounter++;
+
+}
+
+}
+
+Console.WriteLine(spaceCounter + 1);
---
+++ Week6StringsAndArrays/Program.cs
@@ -0,0 +1,27 @@
+﻿//Console.WriteLine("Sisesta lause! :");
+//string hello = Console.ReadLine();
+
+
+string hello = "hello, world! Here I am!";
+
+int stringLength = hello.Length;
+//string trimmedString = hello.Trim();
+
+hello = hello.Trim();
+char firstLetter = hello[0];
+hello = hello.Replace(hello[0], char.ToUpper(hello[0]));
+
+firstLetter = hello[0];
+    
+Console.WriteLine(firstLetter);
+
+
+//int wordCounter = 1;
+//for(int i = 0; i < hello.Length; i++)
+//{
+//    if (char.IsWhiteSpace(hello[i]))
+//    {  wordCounter++;
+//    }
+//}
+
+//Console.WriteLine($"Word count in {hello}: {wordCounter} words.");
